65cc04c04254e41abddfc469b61b7513808f9027
Felix Kiss <felix@felixkiss.com>
2014-08-06 01:46:22 -0400

    DRY up monkey patch of Jekyll::Layout
    This uses an method alias instead of copying the whole code of #initialize.

--- lib/jekyll-haml/ext/convertible.rb
+++ lib/jekyll-haml/ext/convertible.rb
@@ -1,13 +1,11 @@
 # Re-open Layout class to convert our HAML Layout content.
-# This solution redeclares the Layout class in a way that
-# don't risks Jekyll API updates anymore.
-
 module Jekyll
-  class << Layout
+  class Layout
     alias old_initialize initialize
+
     def initialize(*args)
       old_initialize(*args)
-      self.content = self.transform
+      self.transform
     end
   end
 end
