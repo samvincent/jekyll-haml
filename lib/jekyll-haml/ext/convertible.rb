e0d3d0a6cf37f6d33326a31e988e4b033276503f
kalvinarts <kalvinarts@tostadoraplateaa>
2014-09-17 22:33:48 +0200

    Avoid future interference with Jekyll API updates

--- lib/jekyll-haml/ext/convertible.rb
+++ lib/jekyll-haml/ext/convertible.rb
@@ -1,19 +1,12 @@
 # Re-open Layout class to convert our HAML Layout content.
-# This solution risks Jekyll API updates, and potential
-# interference from other included plugins.
+# This solution don't risks Jekyll API updates anymore.
 
 module Jekyll
-  class Layout
-    def initialize(site, base, name)
-      @site = site
-      @base = base
-      @name = name
-
-      self.data = {}
-
-      self.process(name)
-      self.read_yaml(base, name)
-      self.transform
+  class << Layout # Redeclare a copy of the Layout class
+    alias old_initialize initialize
+    def initialize(*args)
+      old_initialize(*args)
+      self.content = self.transform
     end
   end
 end
